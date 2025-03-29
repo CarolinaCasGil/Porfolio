588043f3aaa10a5d0fb0c707548b780dd64b345c
CarolinaCasGil <113991051+CarolinaCasGil@users.noreply.github.com>
2025-03-29 20:31:09 +0100

    Subido parte de estudios

--- Porfolio/src/app/components/studies/studies.component.ts
+++ Porfolio/src/app/components/studies/studies.component.ts
@@ -11,16 +11,18 @@ export class StudiesComponent {
 
 studies = [
     {
-      title: 'Estudio 1',
-      school: 'Esculea 1',
-      date: 'Ayer- Hoy',
-      utilities: ['HTML', 'CSS', 'Java']
+      title: 'GRADO SUPERIOR DISEÑO DE APLICACIONES MULTIPLATAFORMA',
+      school: 'Centro Europeo de Estudios Profesionales',
+      date: '2022-2024',
+      description: 'Durante el Grado Superior en Diseño de Aplicaciones Multiplataforma, he aprendido a desarrollar aplicaciones para diferentes plataformas, como móviles, tabletas y la web. He adquirido conocimientos en programación utilizando lenguajes como Java, HTML y CSS, y también he trabajado con bases de datos y servicios web. Además, he desarrollado habilidades en el diseño de interfaces, análisis de requisitos y gestión de proyectos.',
+      utilities: ['Java', 'HTML', 'CSS', 'SQL', 'Python']
     },
     {
-      title: 'Estudio 2',
-      school: 'Esculea 2',
-      date: 'Ayer- Hoy',
-      utilities: ['HTML', 'CSS', 'Java']
+      title: 'GRADO MEDIO SISTEMAS MICROINFORMÁTICOS Y REDES',
+      school: 'Liceo Sorolla B',
+      description: 'Durante el Grado Medio en Sistemas Microinformáticos y Redes, aprendí a gestionar y mantener sistemas informáticos, tanto hardware como software. Adquirí conocimientos en instalación y configuración de redes, así como en la resolución de problemas técnicos. También me formé en la administración de sistemas operativos y la gestión de servicios de red. ',
+      date: '2020-2022',
+      utilities: ['PHP', 'HTML', 'CSS']
     }
 ]
 
