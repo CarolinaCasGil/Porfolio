9e11e5e94827bbe1ff2aa3f2a5f8923efa73ae21
CarolinaCasGil <113991051+CarolinaCasGil@users.noreply.github.com>
2025-03-30 17:09:41 +0200

    Subido

--- Porfolio/src/app/app.routes.ts
+++ Porfolio/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { DassboardComponent } from './components/dassboard/dassboard.component';
 import { StudiesComponent } from './components/studies/studies.component';
+import { ExperiencesComponent } from './components/experiences/experiences.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/dashboard', pathMatch: 'full' },  // Asegúrate de tener esta ruta
     { path: 'dashboard', component: DassboardComponent },
     { path: 'studies', component: StudiesComponent },
+    { path: 'experiences', component: ExperiencesComponent },
     { path: '**', redirectTo: '/dashboard' }
   ];
