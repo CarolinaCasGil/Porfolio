73cd0342dea8bd626142be4da39e3cb4ba3e5544
CarolinaCasGil <113991051+CarolinaCasGil@users.noreply.github.com>
2025-03-30 23:36:40 +0200

    subido toro responsive

--- Porfolio/src/app/components/experiences/experiences.component.ts
+++ Porfolio/src/app/components/experiences/experiences.component.ts
@@ -14,15 +14,15 @@ export class ExperiencesComponent {
       title: 'Analista superior de datos',
       place: 'ALTEN ',
       date: '2024',
-      description:'-Implementé soluciones de análisis de datos con Google Cloud, Looker Studio y herramientas de BI (Power BI, Python, SQL) que mejoraron la eficiencia operativa en un 10% y redujeron costos en un 15%. -Contribuí a mejorar la toma de decisiones con análisis basados en datos precisos que llevo afluencia en ciertos locales comerciales un 25%.',
+      description:'-Implementé soluciones de análisis de datos con Google Cloud, Looker Studio y herramientas de BI (Power BI, Python, SQL) que mejoraron la eficiencia operativa en un 10% y redujeron costos en un 15%. Contribuí a mejorar la toma de decisiones con análisis basados en datos precisos que llevo afluencia en ciertos locales comerciales un 25%.',
       utilities: ['Google Cloud', 'Power BI', 'Looker (software)', 'SQL', 'Python']
     },
     {
       title: 'Técnico de reparación de equipos informáticos',
       place: 'Inet Smart ITC',
       date: '2022',
-      description:'-Implementé soluciones de análisis de datos con Google Cloud, Looker Studio y herramientas de BI (Power BI, Python, SQL) que mejoraron la eficiencia operativa en un 10% y redujeron costos en un 15%. -Contribuí a mejorar la toma de decisiones con análisis basados en datos precisos que llevo afluencia en ciertos locales comerciales un 25%.',
-      utilities: ['Reparación de equipos informáticos', 'Sistemas informáticos', 'Servicios técnicos', 'Infraestructura de tecnologías de la información', 'Gestión de sistemas']
+      description:'-Implementé soluciones de análisis de datos con Google Cloud, Looker Studio y herramientas de BI (Power BI, Python, SQL) que mejoraron la eficiencia operativa en un 10% y redujeron costos en un 15%. Contribuí a mejorar la toma de decisiones con análisis basados en datos precisos que llevo afluencia en ciertos locales comerciales un 25%.',
+      utilities: ['Reparación de equipos informáticos', 'Sistemas informáticos', 'Servicios técnicos', 'Gestión de sistemas', 'Infraestructura de tecnologías de la información']
     }
   ]
 
