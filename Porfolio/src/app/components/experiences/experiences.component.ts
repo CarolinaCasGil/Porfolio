c5b13345a4aaf364cf3dec837c9c370d4d8e3485
CarolinaCasGil <113991051+CarolinaCasGil@users.noreply.github.com>
2025-03-30 18:25:51 +0200

    subido

--- Porfolio/src/app/components/experiences/experiences.component.ts
+++ Porfolio/src/app/components/experiences/experiences.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-experiences',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './experiences.component.html',
   styleUrl: './experiences.component.scss'
 })
 export class ExperiencesComponent {
+
+  experience = [
+    {
+      title: 'Analista superior de datos',
+      place: 'ALTEN ',
+      date: '2024',
+      description:'-Implementé soluciones de análisis de datos con Google Cloud, Looker Studio y herramientas de BI (Power BI, Python, SQL) que mejoraron la eficiencia operativa en un 10% y redujeron costos en un 15%. -Contribuí a mejorar la toma de decisiones con análisis basados en datos precisos que llevo afluencia en ciertos locales comerciales un 25%.',
+      utilities: ['Google Cloud', 'Power BI', 'Looker (software)', 'SQL', 'Python']
+    },
+    {
+      title: 'Técnico de reparación de equipos informáticos',
+      place: 'Inet Smart ITC',
+      date: '2022',
+      description:'-Implementé soluciones de análisis de datos con Google Cloud, Looker Studio y herramientas de BI (Power BI, Python, SQL) que mejoraron la eficiencia operativa en un 10% y redujeron costos en un 15%. -Contribuí a mejorar la toma de decisiones con análisis basados en datos precisos que llevo afluencia en ciertos locales comerciales un 25%.',
+      utilities: ['Reparación de equipos informáticos', 'Sistemas informáticos', 'Servicios técnicos', 'Infraestructura de tecnologías de la información', 'Gestión de sistemas']
+    }
+  ]
 
 }
